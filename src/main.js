9d15c0da16a0dc2f1bd67a7c82396bedefab4541
Ristafan <martin.faehnrich@gmail.com>
2026-04-14 11:20:29 +0200

    balloon

--- src/main.js
+++ src/main.js
@@ -45,6 +45,14 @@ import './styles/main.css';
         data: data
     });
 
+    createHeatmap({
+        containerId: "#Ballon",
+        svgPath: "/data/images/balloon.svg",
+        acClass: "D",
+        parts: ["STR_RAD", "STR_WINDSHLD", "STR_ENG1", "STR_ENG2", "STR_ENG3", "STR_ENG4", "STR_PROP", "STR_WING_ROT", "STR_FUSE", "STR_LG", "STR_TAIL", "STR_LGHTS"],
+        data: data
+    });
+
     // repeat for other aircrafts
   });
 
